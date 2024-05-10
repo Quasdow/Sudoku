c3a13b1205990edd01a814b496116cd86804e532
quasdow <92190436+Quasdow@users.noreply.github.com>
2024-05-10 23:37:55 +0700

    First Commit

---
+++ src/main/java/model/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package model;
+
+import screen.MenuScreen;
+
+/**
+ *
+ * @author baduy
+ */
+public class App {
+
+    public static void main(String[] args) {
+        MenuScreen sudoku = new MenuScreen();
+        sudoku.setVisible(true);
+        }
+}

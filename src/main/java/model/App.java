40d94fa2dfc7663cf3194802e84a3ddb277904b7
quasdow <92190436+Quasdow@users.noreply.github.com>
2024-05-13 11:05:29 +0700

    Update App.java

--- src/main/java/model/App.java
+++ src/main/java/model/App.java
@@ -1,10 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package model;
 
-import screen.MenuScreen;
+import screen.FormMenu;
 
 /**
  *
@@ -13,7 +12,7 @@ import screen.MenuScreen;
 public class App {
 
     public static void main(String[] args) {
-        MenuScreen sudoku = new MenuScreen();
+        FormMenu sudoku = new FormMenu();
         sudoku.setVisible(true);
-        }
+    }
 }
